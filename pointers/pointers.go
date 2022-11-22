02ec0d953f25f2a6a0d9d7e1773c9696ef2e78d3
x01000101x <leodamanik619@gmail.com>
2022-11-22 15:01:02 +0700

    pointers

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -14,9 +14,11 @@ func main() {
 	i := 1
 	fmt.Println("initial :", i)
 
+	//pemanggilan func biasa
 	zeroval(i)
 	fmt.Println("zeroval :", i)
 
+	//pemanggilan menggunakan pointer, hasilnya kebalikan dari yg biasa
 	zeroptr(&i)
 	fmt.Println("zeroptr :", i)
 
