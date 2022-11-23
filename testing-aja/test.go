16f25084ac39350caa870465dea761d97c1b78d0
x01000101x <leodamanik619@gmail.com>
2022-11-23 09:36:51 +0700

    method

--- testing-aja/test.go
+++ testing-aja/test.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+type orang struct {
+	tinggi int
+	nama   string
+}
+
 func main() {
 	// 	n := 12
 	// 	var r *int = &n
@@ -15,13 +20,24 @@ func main() {
 
 	// }
 
-	var number = 4
-	fmt.Println("before :", number) // 4
+	// 	var number = 4
+	// 	fmt.Println("before :", number) // 4
 
-	change(&number, 10)
-	fmt.Println("after  :", number) // 10
-}
+	// 	change(&number, 10)
+	// 	fmt.Println("after  :", number) // 10
+	// }
 
-func change(original *int, value int) {
-	*original = value
+	// func change(original *int, value int) {
+	// 	*original = value
+
+	x := orang{tinggi: 173, nama: "Leo"}
+	fmt.Println(x)
+
+	var w *orang = &x
+	fmt.Println(w.nama)
+
+	w.nama = "Rizki"
+	fmt.Println(w.nama)
+	fmt.Println(x.nama)
+
 }
