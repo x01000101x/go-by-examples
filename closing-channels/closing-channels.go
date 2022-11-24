f05acd2dc9fa28b7015935f20a3f2794e6f11991
x01000101x <leodamanik619@gmail.com>
2022-11-24 10:32:24 +0700

    closing channels

---
+++ closing-channels/closing-channels.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	jobs := make(chan int, 5)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			j, more := <-jobs
+			if more {
+				fmt.Println("Received job", j)
+			} else {
+				fmt.Println("Received all jobs")
+				done <- true
+				return
+			}
+		}
+	}()
+
+	for j := 1; j <= 3; j++ {
+		jobs <- j
+		fmt.Println("Sent job", j)
+	}
+
+	close(jobs)
+	fmt.Println("sent all jobs")
+	<-done
+
+}
